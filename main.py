e7805712dd774ec7d89fc6d8e73346448764e02b
mirakuru <heniahaikel@gmail.com>
2023-11-10 13:10:29 -0500

    first

---
+++ main.py
@@ -0,0 +1,30 @@
+import click
+
+
+__version__ = "0.1.1-SNAPSHOT"
+
+
+@click.command("bonjour")
+def bonjour():
+    click.echo("bonjour monde")
+
+
+
+@click.command("guten-tag")
+def guten_tag():
+    click.echo("guten tag")
+
+
+
+@click.command("version")
+def version():
+    click.echo(__version__)
+
+
+@click.command("hello")
+def hello():
+    click.echo("Hello, World!")
+
+
+if __name__ == "__main__":
+    hello()
